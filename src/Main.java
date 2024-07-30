899dc8868bda037bb0121624dfbdbf32bcdc0587
Joshua Usi <ExperimentatorsY@gmail.com>
2024-07-30 13:24:23 +1000

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
-        System.out.println("Red vs. Blue");
+        Main window = new Main();
+    }
+
+    class Canvas extends JPanel {
+        public Canvas() {
+            setPreferredSize(new Dimension(720, 720));
+        }
+
+        public void paint(Graphics g) {
+            final int CELL_SIZE = 35;
+            for (int i = 0; i < 20; i++) {
+                for (int j = 0; j < 20; j++) {
+                    g.drawRect(10 + i * CELL_SIZE, 10 + j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                }
+            }
+        }
+    }
+
+    private Main() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+        this.setContentPane(canvas);
+        this.setVisible(true);
+        this.pack();
     }
 }
