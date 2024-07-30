68f8f263534bc4fec204f7ac526858530f13abcc
Joshua Usi <ExperimentatorsY@gmail.com>
2024-07-30 13:33:40 +1000

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,65 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
         Main window = new Main();
     }
 
-    class Canvas extends JPanel {
-        public Canvas() {
-            setPreferredSize(new Dimension(720, 720));
+    class Cell {
+        private int x, y, width;
+
+        public Cell(int x, int y, int width) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
         }
 
         public void paint(Graphics g) {
-            final int CELL_SIZE = 35;
+            g.drawRect(x, y, width, width);
+        }
+    }
+
+    class Grid {
+
+        private int offsetX, offsetY;
+        private ArrayList<Cell> cells;
+
+        public Grid(int offsetX, int offsetY, ArrayList<Cell> cells) {
+            this.offsetX = offsetX;
+            this.offsetY = offsetY;
+            this.cells = cells;
+        }
+
+        public void paint(Graphics g) {
+            g.translate(offsetX, offsetY);
+            for (Cell c : cells) {
+                c.paint(g);
+            }
+            // Move it back to center
+            g.translate(-offsetX, -offsetY);
+        }
+    }
+
+    class Canvas extends JPanel {
+        public Grid grid;
+
+        public Canvas() {
+            setPreferredSize(new Dimension(720, 720));
+
+            ArrayList<Cell> cells = new ArrayList<>();
+            final int CELL_WIDTH = 35;
             for (int i = 0; i < 20; i++) {
                 for (int j = 0; j < 20; j++) {
-                    g.drawRect(10 + i * CELL_SIZE, 10 + j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                    cells.add(new Cell(j * CELL_WIDTH, i * CELL_WIDTH, CELL_WIDTH));
                 }
             }
+            grid = new Grid(10, 10, cells);
+        }
+
+        public void paint(Graphics g) {
+            grid.paint(g);
         }
     }
 
